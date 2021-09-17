764f3e25b61272995ed4e2bd5f113dc1be6fe7f6
zylka44 <zancza@gmail.com>
2021-09-17 18:27:15 +0200

    Init api

---
+++ src/server.ts
@@ -0,0 +1,76 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  updateDoc,
+  doc,
+  addDoc,
+  serverTimestamp,
+  arrayUnion,
+  getDocs,
+  deleteDoc,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC_YhgF4FRS2BGe2EPj6ToDEQHu7cR3oA0',
+  authDomain: 'podchoinke-dd836.firebaseapp.com',
+  databaseURL: 'https://podchoinke-dd836.firebaseio.com',
+  projectId: 'podchoinke-dd836',
+  storageBucket: 'podchoinke-dd836.appspot.com',
+  messagingSenderId: '22205214738',
+  appId: '1:22205214738:web:0c778e8df3ba1756e1a8cb',
+};
+
+const firestore = initializeApp(firebaseConfig);
+const db = getFirestore(firestore);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/products', async (req, res) => {
+  const querySnapshot = await getDocs(collection(db, 'users'));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data().name}`);
+  });
+  res.send('successfuly downloaded');
+});
+
+app.post('/products', async (req, res) => {
+  const data = req.body;
+  await addDoc(collection(db, 'users'), { timestamp: serverTimestamp(), ...data });
+  res.send('ok');
+});
+
+app.patch('/products', async (req, res) => {
+  const data = req.body;
+  const userToUpdate = doc(db, 'users', '0ln9Lmpc7vIPAhyOhh38');
+  //aktualizacja całego dokumentu
+  // await updateDoc(userToUpdate, {timestamp: serverTimestamp(), ...data});
+
+  // aktualizacja jednego pola dolumentu
+  await updateDoc(userToUpdate, {
+    presents: data.presents,
+  });
+
+  // dodawanie elementu do tablicy
+  await updateDoc(userToUpdate, {
+    presents: arrayUnion(data.presents),
+  });
+
+  res.send('seccessfuly updated');
+});
+
+app.delete('/products', async (req, res) => {
+  await deleteDoc(doc(db, 'users', '0ln9Lmpc7vIPAhyOhh38'));
+  res.send('successfuly deleted');
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`api is running at http://localhost:${port}`);
+});
